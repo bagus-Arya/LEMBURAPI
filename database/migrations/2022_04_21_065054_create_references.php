2d2c83dc3f569e7f928d2f14b5dd7747b5dd816c
bagus-Arya <bagusarya850@gmail.com>
2022-04-21 15:18:28 +0700

    add: migration

---
+++ database/migrations/2022_04_21_065054_create_references.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReferences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('references', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+            $table->float('expression', 50, 2)->default(0.0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('references');
+    }
+}
---
+++ database/migrations/2022_04_21_065221_create_overtimes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOvertimes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('overtimes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('employee_id');
+            $table->dateTime('date');
+            $table->dateTime('time_started');
+            $table->dateTime('time_ended');
+            $table->foreign('employee_id')->references('id')->on('employees')
+                    ->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('overtimes');
+    }
+}
